a8d281a4cf0908fa3da581d426a010ed5c7671ab
Mothcam <rinke.vandervelde@hvhl.nl>
2024-05-29 13:23:39 +0200

    Add Tinmelapse_AF.py

--- Timelapse_AF.py
+++ Timelapse_AF.py
@@ -5,19 +5,23 @@ from picamera2 import Picamera2, Preview
 from libcamera import controls
 
 picam2 = Picamera2()
-picam2.configure("still")
+config = picam2.create_still_configuration({"size":(4056, 3040)})
+picam2.configure(config)
+picam2.options["quality"] = 95
 picam2.start()
 
 # Give time for Aec and Awb to settle, before disabling them
 time.sleep(1)
 picam2.set_controls({"AfMode":controls.AfModeEnum.Continuous,"FrameRate": 1.0})
+
 # And wait for those settings to take effect
-time.sleep(1)
+time.sleep(2)
 
 start_time = time.time()
 for i in range(1, 2):
-    r = picam2.capture_request()
-    r.save("main", f"image{i}.jpg")
+    file_path = '/home/Camera/Pictures'
+    r = picam2.capture_request(file_path)
+    r.save("main", f"Image{i}.jpg")
     r.release()
     print(f"Captured image {i} of 50 at {time.time() - start_time:.2f}s")
     time.sleep(5)
