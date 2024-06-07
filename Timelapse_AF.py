a97256a0c8d1f64a1546008658b0a0d5ab7f5d9e
Mothcam <rinke.vandervelde@hvhl.nl>
2024-06-07 10:17:20 +0200

    Update Timelapse_AF.py
    added date into file name

--- Timelapse_AF.py
+++ Timelapse_AF.py
@@ -4,6 +4,8 @@ import time
 from picamera2 import Picamera2
 from libcamera import controls
 
+
+date = time.strft
 picam2 = Picamera2()
 config = picam2.create_still_configuration({"size":(4056, 3040)})
 picam2.configure(config)
@@ -21,7 +23,7 @@ start_time = time.time()
 for i in range(1, 2):
     file_path = '/home/Camera/Pictures'
     r = picam2.capture_request(file_path)
-    r.save("main", f"Image{i}.jpg")
+    r.save("main", f"Cam1{date}{i}.jpg")
     r.release()
     print(f"Captured image {i} of 50 at {time.time() - start_time:.2f}s")
     time.sleep(5)
