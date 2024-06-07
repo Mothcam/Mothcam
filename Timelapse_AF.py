31a3a7fb86043f663e50da7030191bb05456704d
Mothcam <rinke.vandervelde@hvhl.nl>
2024-06-07 15:41:11 +0200

    Update Timelapse_AF.py
    Added # comments in the code

--- Timelapse_AF.py
+++ Timelapse_AF.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
+#import required modules
 import time
 import json
 import sys
 
 from picamera2 import Picamera2
 from libcamera import controls
-
+#Read the config file
 def read_config(file_path):
 	print("File_path:", file_path)
 	try:
@@ -23,32 +24,34 @@ def read_config(file_path):
 	return config
 
 def main():
+	# Take the number of photo's out of the config file. When failed 3 pictures will be taken
 	config_file = '/home/camera/Camera/mothconfig.json'
 	config = read_config(config_file)
-
 	nrfotos = config.get("nrfotos", 3)
+	# Take the camera number out of the config file. When failed the number will be 00
 	file_path = config.get("file_path",'/home/camera/Camera/Pictures')
 	cam_number = config.get("cam_number","00")
-
-	timestr = time.strftime("%d%m%Y")
+	
+#Set variables
+	date = time.strftime("%d%m%Y")
 	picam2 = Picamera2()
 	config = picam2.create_still_configuration({"size":(4056, 3040)})
 	picam2.configure(config)
 	picam2.options["quality"] = 95
 	picam2.start()
 
-# Give time for Aec and Awb to settle, before disabling them
+	# Give time for Aec and Awb to settle, before disabling them and wait for those settings to take effect
 	time.sleep(1)
 	picam2.set_controls({"AfMode":controls.AfModeEnum.Continuous,"FrameRate": 1.0})
 
-# And wait for those settings to take effect
+	#Instructions to the Picamera to take pictures
 	time.sleep(2)
-
 	start_time = time.time()
 
 	for i in range(1, nrfotos + 1):
     		r = picam2.capture_request(file_path)
-    		r.save("main", f"{file_path}/cam{cam_number}-{timestr}-{i}.jpg")
+		#set filename as
+    		r.save("main", f"{file_path}/cam{cam_number}-{date}-{i}.jpg")
     		r.release()
     		print(f"Captured image {i} at {time.time() - start_time:.2f}s")
     		time.sleep(1)
